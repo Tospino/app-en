8692f17378a1d1a0daf9f2cefb22a0470a872535
bebH <49640566+hxw13766443124@users.noreply.github.com>
2020-10-12 09:53:25 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -8,70 +8,69 @@
  */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Vant from 'vant'
-import 'lib-flexible/flexible'
-import '@/assets/css/reset.css'
-import '@/assets/css/cjy.less'
-import 'vant/lib/index.css'
-import '@/assets/css/common.less'
-import '@/assets/css/animation.less'
-import '@/assets/css/cjy.less'
-import './assets/font/font_501137_8uegssallpv/iconfont.css'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Vant from "vant";
+import "lib-flexible/flexible";
+import "@/assets/css/reset.css";
+import "@/assets/css/cjy.less";
+import "vant/lib/index.css";
+import "@/assets/css/common.less";
+import "@/assets/css/animation.less";
+import "@/assets/css/cjy.less";
+import "./assets/font/font_501137_8uegssallpv/iconfont.css";
 
-import vueCountDown from '@liuwave/vue-count-down'
+import vueCountDown from "@liuwave/vue-count-down";
 import {
-    isDisabled,
-    formValidate,
-    strTrim,
-    objTrim,
-    priceReg,
-    objDeepCopy,
-    deepnull,
-    MyLocalStorage
-} from '@/common/utils.js'
-import store from './store'
-import * as mycomponents from '@/common/mycomponents.js'
-import VueScroller from 'vue-scroller'
-import clipboard from 'clipboard'
+  isDisabled,
+  formValidate,
+  strTrim,
+  objTrim,
+  priceReg,
+  objDeepCopy,
+  deepnull,
+  MyLocalStorage
+} from "@/common/utils.js";
+import store from "./store";
+import * as mycomponents from "@/common/mycomponents.js";
+import VueScroller from "vue-scroller";
+import clipboard from "clipboard";
 
-import { gethttpimgurlApi } from '@/api/login/index'
+import { gethttpimgurlApi } from "@/api/login/index";
 
-
-Vue.prototype.$webUrl = ''
-Vue.prototype.jn = ''
+Vue.prototype.$webUrl = "";
+Vue.prototype.jn = "";
 gethttpimgurlApi().then(res => {
-    if (res.code == 0) {
-        Vue.prototype.jn = res.currency
-        Vue.prototype.$webUrl = res.webUrl + 'thumb/'
-        Vue.prototype.$bigwebUrl = res.webUrl
-    }
-})
-Vue.use(vueCountDown, { component: 'count-down' })
-Vue.use(VueScroller)
-Vue.use(Vant)
+  if (res.code == 0) {
+    Vue.prototype.jn = res.currency;
+    Vue.prototype.$webUrl = res.webUrl + "thumb/";
+    Vue.prototype.$bigwebUrl = res.webUrl;
+  }
+});
+Vue.use(vueCountDown, { component: "count-down" });
+Vue.use(VueScroller);
+Vue.use(Vant);
 
 Vue.prototype.$fn = {
-    copy: objDeepCopy,
-    isDisabled,
-    formValidate,
-    strTrim,
-    objTrim,
-    priceReg,
-    deepnull,
-    MyLocalStorage
-}
+  copy: objDeepCopy,
+  isDisabled,
+  formValidate,
+  strTrim,
+  objTrim,
+  priceReg,
+  deepnull,
+  MyLocalStorage
+};
 
-Vue.prototype.clipboard = clipboard
+Vue.prototype.clipboard = clipboard;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 //注册全局组件
 Object.keys(mycomponents).forEach(key => {
-    Vue.component(key, mycomponents[key])
-})
+  Vue.component(key, mycomponents[key]);
+});
 
 // router.afterEach((to,from,next) => {
 //   window.scroll(200, 200);
@@ -79,32 +78,32 @@ Object.keys(mycomponents).forEach(key => {
 // });
 
 // 易观数据分析
-import AnalysysAgent from 'ans-javascript-sdk/SDK/AnalysysAgent_JS_SDK.es6.min.js'
+import AnalysysAgent from "ans-javascript-sdk/SDK/AnalysysAgent_JS_SDK.es6.min.js";
 AnalysysAgent.init({
-    appkey: 'b9a147c9ff75c354',//APPKEY
-    uploadURL: 'http://124.156.105.108:8089',//上传数据的地址
-    SDKFileDirectory: 'http://124.156.105.108:8089',//可视化与热图模块SDK存放目录。
-    debugMode: 0
-})
+  appkey: "b9a147c9ff75c354", //APPKEY
+  uploadURL: "http://124.156.105.108:8089", //上传数据的地址
+  SDKFileDirectory: "http://124.156.105.108:8089", //可视化与热图模块SDK存放目录。
+  debugMode: 2
+});
 
 // 引入图片懒加载
-import VueLazyload from 'vue-lazyload'
+import VueLazyload from "vue-lazyload";
 Vue.use(VueLazyload, {
-    preLoad: 1.3,
-    error: '../static/goods_default.jpg', // 加载失败或者无资源时显示的图片
-    loading: '../static/goods_default.jpg', // loading图片，未加载时显示的
-    attempt: 1
-})
+  preLoad: 1.3,
+  error: "../static/goods_default.jpg", // 加载失败或者无资源时显示的图片
+  loading: "../static/goods_default.jpg", // loading图片，未加载时显示的
+  attempt: 1
+});
 // 引入缓存插件
-import storage from 'storejs';
-Vue.prototype.$storage = storage
+import storage from "storejs";
+Vue.prototype.$storage = storage;
 
 /* eslint-disable no-new */
 const main = new Vue({
-    el: '#app',
-    router,
-    store,
-    components: { App },
-    template: '<App/>'
-})
-export default main
+  el: "#app",
+  router,
+  store,
+  components: { App },
+  template: "<App/>"
+});
+export default main;
