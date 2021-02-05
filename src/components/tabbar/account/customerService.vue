8d01c5a759d5fd5448a8c89d4810b9cb8a63110b
meter CJY <854916013@qq.com>
2021-02-05 11:19:53 +0800

    2021年2月5日11:19:52

--- src/components/tabbar/account/customerService.vue
+++ src/components/tabbar/account/customerService.vue
@@ -2,7 +2,7 @@
  * @Author: 曹建勇
  * @Date: 2020-07-30 13:39:53
  * @LastEditors: 曹建勇
- * @LastEditTime: 2021-01-09 09:30:11
+ * @LastEditTime: 2021-02-05 11:19:17
  * @Description: 
  * @FilePath: \app-en\src\components\tabbar\account\customerService.vue
 --> 
@@ -110,7 +110,7 @@ export default {
     // },
     getData() {
       let otherParams;
-      let nickName = `app user ID:${this.userinfoShop.userId}`;
+      let nickName = `app user ID C:${this.userinfoShop.userId}`;
       this.userinfoShop.userId;
       if (this.type === 0) {
         //   普通聊天
